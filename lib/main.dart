5e09b9042013e2158c4bfd645e595aea26ebd693
TamNhiDuong <yanxinyee@gmail.com>
2026-03-16 14:33:34 +0200

    Implemented basic UI

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'controllers/note_controller.dart';
+import 'screens/note_screen.dart';
+
+void main() {
+  Get.lazyPut<NoteController>(() => NoteController());
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'My notes', home: NoteScreen());
+  }
+}
